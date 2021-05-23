2d1c382d9ecba392b8d7c99849769ceabaddad10
ValenteXD <jpcovello@hotmail.com>
2021-05-23 12:27:06 -0300

    Update menu.lua

--- menu.lua
+++ menu.lua
@@ -1,13 +1,20 @@
 local menu={}
-function mouse_no_botao(mx,my,x,y,w,h)
+local function mouse_no_botao(mx,my,x,y,w,h)
   return mx>=x and mx<=x+w and my>=y and my<=y+h
 end
-local function botao(x,y)
+local function start(x,y)
   love.graphics.setColor(0,1,0)
   love.graphics.rectangle('fill',x-60,y-40,120,60)
   love.graphics.setColor(1,1,1)
   love.graphics.setFont(fonte)
   love.graphics.print('start',x-55,y-40)
+end
+local function exit(x,y)
+  love.graphics.setColor(1,0,0)
+  love.graphics.rectangle('fill',x-60,y-40,120,60)
+  love.graphics.setColor(1,1,1)
+  love.graphics.setFont(fonte)
+  love.graphics.print('exit',x-45,y-37)
 end
 function menu.load()
   fonte = love.graphics.newFont(45)
@@ -16,7 +23,8 @@ function menu.update(dt)
   
 end
 function menu.draw()
-  botao(400,300)
+  start(400,300)
+  exit(400,370)
 end
 function menu.keypressed(key)
   
@@ -24,8 +32,9 @@ end
 function menu.mousepressed(x,y,button)
   if button == 1 and mouse_no_botao(x,y,360,240,80,120) then
     vai_para_sim()
-    sucesso=true
+  end
+  if button == 1 and mouse_no_botao(x,y,360,310,80,120) then
+    love.event.quit()
   end
 end
-
 return menu
