243c0018c5eadda310e8b788098ac0842d65d966
ValenteXD <82062623+ValenteXD@users.noreply.github.com>
2021-06-04 15:17:33 -0300

    Add files via upload

---
+++ fisica.lua
@@ -0,0 +1,38 @@
+local fisica = {}
+local g = 9.8
+function fisica.peso(m)
+  
+  return m * g
+  
+  ---resultado em N-
+end
+function fisica.tracao(McontP, acel)
+  
+  return McontP * acel + fisica.peso(McontP)
+  
+  --resultado em N--
+end
+function fisica.forcaMotor(McontP, Melev, acel)
+  
+  return Melev * acel + fisica.peso(Melev) - fisica.tracao(McontP, acel)
+  
+  --resultado em N--
+end
+function fisica.potencia(Vel, McontP, Melev, acel)
+  
+  return fisica.forcaMotor(McontP, Melev, acel) * Vel
+  
+  --resultado em W--
+end
+function fisica.energia(Vel,dt, McontP, Melev, acel)
+  
+  return fisica.potencia(Vel, McontP, Melev, acel) * dt / 1000 * 3600
+  
+  --resultado em kWh--
+end
+  
+  return (McontP - Melev) / (McontP + Melev) * g
+  
+  --resultado em m/s^2--
+end
+return fisica
